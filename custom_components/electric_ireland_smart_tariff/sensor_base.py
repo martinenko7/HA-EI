90eb9688359b543f392cdf1fc88f4e1c37c2927a
martinenko7 <mkrivosik@gmail.com>
2025-12-21 12:43:41 +0000

    fix format error

--- custom_components/electric_ireland_smart_tariff/sensor_base.py
+++ custom_components/electric_ireland_smart_tariff/sensor_base.py
@@ -219,7 +219,7 @@ class Sensor(PollUpdateMixin, HistoricalSensor, SensorEntity):
     def statistic_id(self) -> str:
         return self.entity_id
 
-            def get_statistic_metadata(self) -> StatisticMetaData:
+                def get_statistic_metadata(self) -> StatisticMetaData:
         """
         Add sum and mean to base statistics metadata.
         Updated to comply with Home Assistant 2024.11+ requirements.
@@ -240,6 +240,7 @@ class Sensor(PollUpdateMixin, HistoricalSensor, SensorEntity):
             meta["unit_class"] = "monetary"
 
         return meta
+
 
 
     async def async_calculate_statistic_data(
